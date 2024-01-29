4be0d990bb2005ff858eb9ecec39000e8a0e630c
Crowdbotics <team@crowdbotics.com>
2024-01-29 04:22:37 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "autotrader-automatc-45644",
+  url: "https://autotrader-automatc-45644.botics.co",
+  api: "https://autotrader-automatc-45644.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
